a1ff4fa1359d25f20d00da7ed71d2ae2acc016c4
ECD\dparsa <danial.parsa@experianinteractive.com>
2020-01-29 16:58:50 -0800

    connect to db and models

--- c and c/AntigenPage.xaml.cs
+++ c and c/AntigenPage.xaml.cs
@@ -24,5 +24,7 @@ namespace CC
         {
             InitializeComponent();
         }
+
+
     }
 }
--- c and c/Providers/AntigensProvider.cs
+++ c and c/Providers/AntigensProvider.cs
@@ -8,6 +8,46 @@ namespace CC.Providers
 {
     public class AntigensProvider
     {
+        Entities dbcontext = new Entities();
+
+        public bool CreateAntigen(Antigen antigen)
+        {
+            try
+            {
+                dbcontext.Antigens.Add(antigen);
+                dbcontext.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log somewhere
+                return false;
+            }
+        }
+
+        public bool UpdateAntigen(Antigen antigen)
+        {
+            try
+            {
+                var antigenToUpdate = dbcontext.Antigens.FirstOrDefault(a => a.AntigenId == antigen.AntigenId);
+
+                antigenToUpdate.AntigenName = antigen.AntigenName;
+                antigenToUpdate.UpdatedBy = antigen.UpdatedBy;
+                antigenToUpdate.UpdatedDt = DateTime.Now;
+
+                dbcontext.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log somewhere
+                return false;
+            }
+        }
+
+
 
     }
 }
